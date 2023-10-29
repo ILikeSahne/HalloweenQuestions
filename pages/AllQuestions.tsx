8f6005a8139aa80b93da5932d246e501cbe830ab
ILikeSahne <jonathan.jeszenszky@gmail.com>
2023-10-29 19:50:49 +0100

    Questions are now shuffled

--- pages/AllQuestions.tsx
+++ pages/AllQuestions.tsx
@@ -6,12 +6,30 @@ import { GetServerSideProps, InferGetServerSidePropsType } from "next";
 import "../styles/SetupWrapper.css"
 
 export async function getServerSideProps() {
-    const questions = await prisma.questions.findMany();
+    const questions = shuffle(await prisma.questions.findMany());
 
     return {
         props: { questions: questions }
     };
 }
+
+function shuffle<T>(array: T[]): T[] {
+    let currentIndex = array.length,  randomIndex;
+
+    // While there remain elements to shuffle.
+    while (currentIndex != 0) {
+  
+      // Pick a remaining element.
+      randomIndex = Math.floor(Math.random() * currentIndex);
+      currentIndex--;
+  
+      // And swap it with the current element.
+      [array[currentIndex], array[randomIndex]] = [
+        array[randomIndex], array[currentIndex]];
+    }
+  
+    return array;
+};
 
 export default function AllQuestionsPage({
     questions
