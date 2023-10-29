a65b0eb96a2883a8d28fee0cee1ccff25ea958ad
ILikeSahne <jonathan.jeszenszky@gmail.com>
2023-10-29 19:53:15 +0100

    better page seperation

--- components/Questions.tsx
+++ components/Questions.tsx
@@ -13,7 +13,7 @@ export default function Questions({questions} : IQuestions) {
     <div style={{ display: "flex", alignItems: "center", flexDirection: "column", margin: '16px'}} >
       {questions.map((question, i) => {
         return (
-          <div key={question.id} style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", pageBreakAfter: i % 6 == 5 ? 'always' : "auto" }}>
+          <div key={question.id} style={{ display: "flex", alignItems: "center", justifyContent: "center", flexDirection: "column", pageBreakAfter: i % 5 == 4 ? 'always' : "auto" }}>
             <p style={{ fontSize: "40px", margin: "0", textAlign: "center" }}>{question.question}</p>
             <p style={{ fontSize: "25px", opacity: '0.5', margin: "0px 0px 60px 0px" }}>Geht an {question.goes_to}</p>
           </div>
